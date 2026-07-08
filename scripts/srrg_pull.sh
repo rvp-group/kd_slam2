a725da66a33371e1be376c9cf1f896e451b80992
Giorgio Grisetti <grisetti@dis.uniroma1.it>
2026-07-08 16:18:28 +0200

    polishing

--- scripts/srrg_pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Pull srrg dependencies into the current ROS2 workspace src directory.
-# Run from ~/ws/src after cloning kd_slam2 and kd_slam2_msgs.
-
-set -e
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-vcs import < "$SCRIPT_DIR/../docker/repos.yml"
